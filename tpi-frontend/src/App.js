7a92fa71653e3ad0e381b2bc8a30c7489983784b
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2024-07-28 00:58:29 -0300

    Borramos el inicio que no sirve

--- tpi-frontend/src/App.js
+++ tpi-frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Inicio from "./components/Inicio";
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 
@@ -13,7 +12,6 @@ return (
             <Menu/>
             <div className="divBody">
                 <Routes>
-                    <Route path="/inicio" element={<Inicio/>}/>
                     <Route path="/misiones" element={<Misiones/>}/>
                     <Route path="*" element={<Navigate to='/misiones' replace/>}/>
                 </Routes>
